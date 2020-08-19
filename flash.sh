2169b59ae1b427edf41256e0c4d4fbad99076ec1
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2020-08-19 14:56:44 +0300

    up

--- flash.sh
+++ flash.sh
@@ -7,7 +7,10 @@ make v60_type_r:uran
 
 # dfu-programmer is called under current user, not root.
 # for dfu device to be writable to user, add the following line to /etc/udev/rules.d/20-hw1.rules
-# SUBSYSTEMS=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2ff4", MODE="0660", TAG+="uaccess", TAG+="udev-acl”
+# SUBSYSTEMS=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2ff4", MODE="0660", GROUP="users"
+
+# To enter flash mode on the keyboard, press the reset button on the back cover. You can press it
+# while running this script.
 
 
 while ! dfu-programmer atmega32u4 erase
--- keyboards/v60_type_r/keymaps/uran/keymap.c
+++ keyboards/v60_type_r/keymaps/uran/keymap.c
@@ -27,17 +27,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------------------------------------------------------|
      * |FN0   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return  |
      * |-----------------------------------------------------------|
-     * |LShift  |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /| RShift   |
+     * |LShift  |  Z|  X|  C|  V|  B|  N|M/L3| ,|  .|  /| RShift   |
      * |-----------------------------------------------------------|
-     * |Ctrl|Alt |Gui |      Space/L3           |Gui |Alt |Ctrl|Fn1|
+     * |Ctrl|Alt |Gui |      Space              |Gui |Alt |Ctrl|Fn1|
      * `-----------------------------------------------------------'
      */
     [0] = LAYOUT_all(
         KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  XXXXX, KC_BSPC, \
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,     KC_BSLS, \
         LT(1, KC_CAPS),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  \
-        KC_LSFT, XXXXX,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, XXXXX,  \
-        KC_LCTL, KC_LALT, KC_LGUI,               LT(2, KC_SPC),                          KC_RGUI, KC_RALT,  KC_RCTL, MO(3)),
+        KC_LSFT, XXXXX,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N, LT(2, KC_M),    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, XXXXX,  \
+        KC_LCTL, KC_LALT, KC_LGUI,                      KC_SPC,                          KC_RGUI, KC_RALT,  KC_RCTL, MO(3)),
     /* Keymap 1: FN Layer
      * ,-----------------------------------------------------------.
      * |   | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|  Del  |
@@ -52,30 +52,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------'
      */
     [1] = LAYOUT_all(
-        _____,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  _____,  KC_DEL, \
-        _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____, KC_PSCR, KC_SLCK, KC_PAUS,          KC_INS, \
+        KC_GRAVE,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  _____,  KC_DEL, \
+        _____,   _____,   _____,   RGB_TOG,  _____,   _____,   _____,   _____,   _____,   _____, KC_PSCR, KC_SLCK, KC_PAUS,          KC_INS, \
         _____,   _____,   _____,   _____,   _____,   _____, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_HOME, KC_PGUP,           _____,          \
-        _____,   _____,   _____,   _____,   _____,   _____,   _____, KC_MUTE, KC_VOLD, KC_VOLU,  KC_END, KC_PGDN,           _____,  _____,  \
+        _____,   _____,   _____,   _____,   _____,   _____, KC_VOLD, KC_VOLU, KC_MUTE,  KC_END, KC_PGDN, _____,          _____,  _____,  \
         _____,   _____,   _____,               _____,                                                      _____,   _____,  _____,  _____),
     /* Keymap 2: Mouse Keys Layer
      * ,-----------------------------------------------------------.
      * |   |   |   |   |   |   |   |   |   |   |   |   |   |       |
      * |-----------------------------------------------------------|
-     * |     |   |   |   |   |   |   |MWU|MWD|   |   |   |   |     |
+     * |     |MWU|MUp|MWLt|MWRt|   |   |  |  |   |   |   |   |     |
      * |-----------------------------------------------------------|
-     * |      |   |   |   |   |   |MLt|MDn|MUp|MRt|   |   |        |
+     * |      |MLt|MDn|MRt|   |   |Lt | Dn| Up| Rt|   |   |        |
      * |-----------------------------------------------------------|
-     * |        |   |   |   |   |SPC|Ms1|Ms2|Ms3|   |   |          |
+     * |        |MWD|   |   |   |   |Ms1|   |Ms2|Ms3|Ms4|          |
      * |-----------------------------------------------------------|
      * |    |    |    |                        |    |    |    |    |
      * `-----------------------------------------------------------'
      */
     [2] = LAYOUT_all(
-        _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,  _____,  _____,  _____,  _____,  _____, \
-        _____,   _____,   _____,   _____,   _____,   _____,   _____,   KC_MS_WH_UP, KC_MS_WH_DOWN,   _____, _____, _____, _____,          _____, \
-        _____,   _____,   _____,   _____,   _____,   _____, KC_MS_LEFT, KC_MS_DOWN, KC_MS_UP, KC_MS_RIGHT, _____, _____,           _____,          \
-        _____,   _____,   _____,   _____,   _____,   _____,   KC_SPC, KC_MS_BTN1, KC_MS_BTN2, KC_MS_BTN3,  _____, _____,           _____,  _____,  \
-        _____,   _____,   _____,               _____,                                                      _____,   _____,  _____,  _____),
+        _____,   _____,            _____,        _____,         _____,          _____, _____,   _____,   _____,   _____,  _____,  _____,  _____,  _____,  _____, \
+        _____,   KC_MS_WH_UP,      KC_MS_UP,     KC_MS_WH_LEFT, KC_MS_WH_RIGHT, _____, _____,   _____, _____,   _____, _____, _____, _____,          _____, \
+        _____,   KC_MS_LEFT,       KC_MS_DOWN,   KC_MS_RIGHT,   _____,          _____, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _____, _____,           _____,          \
+        _____,_____,KC_MS_WH_DOWN, _____,         _____,          _____, _____, KC_MS_BTN1, _____, KC_MS_BTN2,  KC_MS_BTN3, KC_MS_BTN4, _____, _____,   \
+        _____,   _____,       _____,               _____,                                                      _____,   _____,  _____,  _____),
     /* Keymap 3: LED Modification Layer
      * ,-----------------------------------------------------------.
      * |   |BLT|BLC|UGT|UGP|UGB|UGR|   |   |   |   |   |   |       |
@@ -90,10 +90,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------'
      */
     [3] = LAYOUT_all(
-        _____, BL_TOGG, BL_STEP, RGB_TOG, RGB_M_P, RGB_M_B, RGB_M_R,   _____,   _____,   _____,   _____,   _____,   _____,  _____,  _____, \
-        _____, RGB_RI,  RGB_GI,  RGB_BI,  RGB_HUI, RGB_SAI, RGB_VAI,   _____,   _____,   _____,   _____,   _____,   _____,          _____, \
-        _____, RGB_RD,  RGB_GD,  RGB_BD,  RGB_HUD, RGB_SAD, RGB_VAD,   _____,   _____,   _____,   _____,   _____,           _____,          \
-        _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,           _____,  _____,  \
+        _____, BL_TOGG, BL_STEP, RGB_TOG, RGB_M_P, RGB_M_B, RGB_M_R,   RGB_M_SW,   RGB_M_SN,   RGB_M_K,   RGB_M_X,   RGB_M_G,   RGB_M_T,  _____,  _____, \
+        _____, _____,  _____,  RGB_TOG,  RGB_RI, RGB_GI, RGB_BI,   RGB_HUI,   RGB_SAI,   RGB_VAI,   _____,   _____,   _____,          _____, \
+        _____, _____,  _____,  _____,    RGB_RD, RGB_GD, RGB_BD,   RGB_HUD,   RGB_SAD,   RGB_VAD,   _____,   _____,           _____,          \
+        _____,   _____,   _____,   BL_STEP,   _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,           _____,  _____,  \
         _____,   _____,   _____,               _____,                                                      _____,   _____,  _____,  _____),
 };
 
